cabbcb577b4a53b9a68975e7819cfb9b7cd4c93c
Danielle-91 <danielle.alamilla@gmail.com>
2023-01-30 17:04:54 -0500

    input connected to category btns

--- src/components/UI/NewItemModule.js
+++ src/components/UI/NewItemModule.js
@@ -1,21 +1,29 @@
+import { useState } from "react";
+
 function NewItemModule() {
 
+    const [enteredTask, setEnteredTask] = useState('');
+
+    const handleNewTask = (e) => {
+        setEnteredTask(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(enteredTask)
+    }
     return (
         <div className="template">
             <div className="addItem">
-                <form action="">
+                <form action="" name="newItem" onSubmit={handleSubmit}>
                     <label htmlFor="newItem">What's on the Agenda?</label>
-                    <input type="text" id="task" name="task" />
-                    <div className="categories">
-                        <div className="left">
-                            <button>Top Priorities</button>
-                            <button>Reminders</button>
-                        </div>
-                        <div className="right">
-                            <button>Notes</button>
-                            <button>To Do</button>
-                        </div>
+                    <input type="text" id="task" name="task" value={enteredTask} onChange={handleNewTask} />
 
+                    <div className="catBtns">
+                        <button type="submit">Top Priorities</button
+                        ><button type="submit">Reminders</button>
+                        <button type="submit">Notes</button>
+                        <button type="submit">To Do</button>
                     </div>
 
                 </form>
--- src/components/notepads/Notes.js
+++ src/components/notepads/Notes.js
@@ -2,6 +2,12 @@ function Notes(){
     return(
         <div className="notes">
             <h2>Notes</h2>
+
+            <ul>
+                <li>Walk dog</li>
+                <li>Buy lettuce</li>
+                <li>Vengeance</li>
+            </ul>
         </div>
     )
 }
