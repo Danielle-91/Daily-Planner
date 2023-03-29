f6ef062e0abd74c1690e40de439f2a5ad02c4c98
Danielle-91 <danielle.alamilla@gmail.com>
2023-03-29 16:28:43 -0400

    inputs appearing out of order, reconfiguring to save array instead of object

--- src/components/UI/NewItemModule.js
+++ src/components/UI/NewItemModule.js
@@ -7,41 +7,14 @@ import {firebase} from '../../firebase';
 // when user is stored, save object for individual person
 // break down to notebooks
 // when user adds to database
-function NewItemModule(props) {
-
-    const [enteredTask, setEnteredTask] = useState('');
-
-    const handleNewTask = (e) => {
-        setEnteredTask(e.target.value)
-    }
-    
-
-    const prioritiesBtn = (e) => {
-        console.log('priorities')
-    }
-
     // look at docs
 
     // click to add, see 4 options, when user clicks reminder e.g., showcases the form
     // if reminder selected, show that form, scope on function to it
     // typepOfTask = db.filter(reminders) or something
-    const remindersBtn = (e) => {
-        // setEnteredTask(e.target.value)
-        // event.preventDefault prevents the default action (form submission and page refresh)
-        e.preventDefault();
-        setEnteredTask(e.target.value)
-        
-    }
 
-    const notesBtn = (e) => {
-        console.log('notes')
-    }
+function NewItemModule(props) {
 
-    const todoBtn = (e) => {
-        return (
-            <p>{enteredTask}</p>
-        )
-    }
     return (
         <>
             <div className="template">
--- src/components/notepads/TopPriorities.js
+++ src/components/notepads/TopPriorities.js
@@ -8,6 +8,11 @@ function TopPriorities() {
 
     const [newTask, setNewTask] = useState("");
     const [priorities, setPriorities] = useState([])
+    const [showTemplate, setShowTemplate] = useState(false);
+
+    const handleTemplate = (e) => {
+        setShowTemplate(!showTemplate)
+    }
 
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
@@ -27,16 +32,16 @@ function TopPriorities() {
 
     const createNewTask = (e) => {
         e.preventDefault();
+        const uidd = uid();
 
-        if(newTask === ""){
+        if (newTask === "") {
             alert('please enter a valid task')
         }
 
-        const uidd = uid();
         set(ref(db, `${auth.currentUser.uid}/${uidd}`), {
             newTask: newTask,
             uidd: uidd,
-        })
+    })
 
         setNewTask("");
     }
@@ -48,36 +53,44 @@ function TopPriorities() {
     return (
 
         <div className="priorities">
-            <h2>Top Priorities</h2>
+            <div className="titleFlex">
+                <h2>Top Priorities</h2>
 
-            <input type="checkbox" className="toggle" />
-            <div className="addBtn">
-                <button><i className="fa-solid fa-plus"></i></button>
+                <input type="checkbox" className="toggle" />
+                <button className='addBtn' onClick={handleTemplate}><i className="fa-solid fa-plus"></i></button>
             </div>
 
-            <form action="" name="newItem">
 
-                <label htmlFor="newItem">Add a new Top Priority</label>
-                <input type="text"
-                    id="task"
-                    name="task"
-                    value={newTask}
-                    onChange={handleChange} />
+            {showTemplate === true ?
+                <div className="template">
+                    <div className="addItem">
+                        <form action="" name="newItem">
 
-                <button type="submit" onClick={createNewTask}>Add</button>
-            </form>
+                            <label htmlFor="newItem">Add a new Top Priority</label>
+                            <input type="text"
+                                id="task"
+                                name="task"
+                                value={newTask}
+                                onChange={handleChange} />
+
+                            <button type="submit" onClick={createNewTask}>Add</button>
+                        </form>
+                    </div>
+                </div> :
+                null
+            }
+
+
 
             <div className="taskList">
                 {
-                priorities.map((priority) => (
-                    <ul>
-                        <li key={newTask.id}>{priority.newTask}</li>
-                    </ul>
-                    
-                ))
-            }
+                    priorities.map((priority) => (
+                        <ul>
+                            <li key={newTask.uidd}>{priority.newTask}</li>
+                        </ul>
+                    ))
+                }
             </div>
-            
 
         </div>
     )
